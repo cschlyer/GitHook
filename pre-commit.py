6cad299e3dcfd0bd9f8923d9e97e459fe38d5001
Chris Schlyer <cschlyer@gmail.com>
2016-04-21 21:58:38 -0700

    yep, more garbage

--- pre-commit.py
+++ pre-commit.py
@@ -5,9 +5,7 @@ import humanhash
 
 # get the last commit SHA and print it after humanizing it
 # https://github.com/zacharyvoase/humanhash
-print humanhash.humanize(
-    subprocess.check_output(
-        ['git','rev-parse','HEAD']))
+print ('testing')
 
 # some commit
 #some other commit
